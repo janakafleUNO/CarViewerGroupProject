f0a2d235118ea576c4dd73965a512ff6d09d6ab8
janakafleUNO <jkafle@unomaha.edu>
2022-03-23 18:12:01 -0500

    Aded the New Images for Brands And Review

--- GroupProject/settings.py
+++ GroupProject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@_cn#h5g8)#y#pxnlu!2l3_d6n%u!s=4g0wa_j48o$iu^rqc1a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
